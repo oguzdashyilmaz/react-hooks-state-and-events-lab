4a1b9f6c936063d7d7e2dc4df9dd123124097aa1
Oguz <yilmazoguz@outlook.com>
2022-02-02 16:04:12 +0300

    Activate add to cart button changing css of class of it

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
 import React from "react";
+import {useState} from "react";
 
 function Item({ name, category }) {
+  
+  const [toCart, setToCart] = useState(false);
+
+  function handleClick() {
+    setToCart(toCart => !toCart)
+  };
+
+
+
   return (
-    <li className="">
+    <li className={toCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>Add to Cart</button>
     </li>
   );
 }
